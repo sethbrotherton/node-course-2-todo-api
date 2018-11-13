0056d5debab3e61e1c615e123bbe828ae1af6f0c
Seth - home computer <sethbrotherton@yahoo.com>
2018-11-13 02:03:22 -0500

    added testing for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -40,8 +40,6 @@ app.get("/todos", (req, res) => {
 app.get("/todos/:id", (req, res) => {
   var id = req.params.id;
 
-  // VAlid id using isValid
-  // 404 - send back empty send
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
@@ -57,13 +55,6 @@ app.get("/todos/:id", (req, res) => {
     .catch(e => {
       res.status(400).send();
     });
-
-  // findById
-  // success
-  // if todo -send it back
-  // if no todo - seck back 404 with empty body
-  // error
-  // 400 - and send empty body back
 });
 
 app.listen(3000, () => {
